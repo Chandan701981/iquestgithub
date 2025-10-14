bb22cb4c0aa57b7fbffdc2d20fb2f4bf0b57ddfd
Admin <Admin@DESKTOP-LQFA8J7>
2025-10-14 07:23:53 +0530

    operator basics

---
+++ python Tutorials/basics/Operator_01.py
@@ -0,0 +1,23 @@
+'''
+Created on 14-Oct-2025
+
+@author:chandan
+operater : is a symbol which perform operation on one or more operands(variables)
+
+I  classificztion of operaters based on number of operands
+    
+    1 unary operator : ats on two operands
+    Ex: =,-
+    2. Binary operator: acts on two operands
+    Ex:+,-etc
+    3. Ternary operator:ats on three operands 
+    Ex: list comprehension
+    
+    
+  
+    II classifiction of operators based on operations
+     1 Arthmetic operators: +,-,*,/(quotient),%(Remainder),**(Exponent),//(Floor division)
+     2.Comparision/Relation operators :<,>,<=,>=,!=,==
+     3.Assignment operator:
+
+'''
