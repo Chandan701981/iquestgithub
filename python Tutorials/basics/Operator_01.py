8085bfb8c287bf71b916e63a3c442c3b24e0a4c1
Admin <Admin@DESKTOP-LQFA8J7>
2025-10-15 17:07:24 +0530

    Operator_01 done

--- python Tutorials/basics/Operator_01.py
+++ python Tutorials/basics/Operator_01.py
@@ -17,7 +17,97 @@ I  classificztion of operaters based on number of operands
   
     II classifiction of operators based on operations
      1 Arthmetic operators: +,-,*,/(quotient),%(Remainder),**(Exponent),//(Floor division)
+     i/p:numerical
      2.Comparision/Relation operators :<,>,<=,>=,!=,==
+     i/p:numerical
+     o/p:boolean
      3.Assignment operator:
-
+     4.logical operator :AND,OR,NOTboolean
+     
+     AND(both):if 'a' AND 'b' is true.takes 2i/p
+     OR
+     i/p:boolean(true/false)
+     o/p:
+     5.Membership operator:to check whether any element is part of a group
+     Ex:in,not in
+     6.Identity operator:To check identity /to check whether 2 variables are identical
+     Ex:is,isnot
+     7.unary minus operator:to negate the numbers
 '''
+print("=======Arithmetic Operators=========")
+a = 20
+b = 3
+print("a+b:", a+b)
+print("a-b:", a-b)
+print("a*b:", a*b)
+print("a/b:", a/b)
+print("20.0/3:", 20.0/3)
+print("a%b:", a%b)
+print("a**b:", a**b)
+print("a//b:", a//b)
+print("20.0//3:", 20.0//3)
+
+print("========Comparison/ Relational Operators=======")
+print("a>b:", a>b)
+print("a<b:", a<b)
+print("a>=b:", a>=b)
+print("a<=b:", a<=b)
+print("a==b:", a==b)
+print("a!=b:", a!=b)
+
+print("=========Logical Operators===========")
+print("=====AND Operator=====")
+print("True and True:", True and True)
+print("True and False:", True and False)
+print("False and True:", False and True)
+print("False and False:", False and False)
+
+print("=====OR Operator=====")
+print("True or True:", True or True)
+print("True or False:", True or False)
+print("False or True:", False or True)
+print("False or False:", False or False)
+
+print("======NOT Operator======")
+print("not True: ", not True)
+print("not False: ", not False)
+
+print("======Membership Operators======")
+print("'i' in 'chandan':",'i' in 'chandan')
+print("'I' in 'chandan':",'I' in 'chandan')
+print("'z' in 'chandan':",'z' in 'chandan')
+print("'z' not in 'chandan':",'z' not in 'chandan')
+
+print("======Identity Operators======")
+name1 = 'chandan'
+name2 = 'chandan'
+print(id(name1))
+print(id(name2))
+
+print("name1 is name2: ", name1 is name2)
+print("name1 is not name2: ", name1 is not name2)
+print("=======Arithmetic Operators=========")
+a = 20
+b = 3
+print("a+b:", a+b)
+print("a-b:", a-b)
+print("a*b:", a*b)
+print("a/b:", a/b)
+print("20.0/3:", 20.0/3)
+print("a%b:", a%b)
+print("a**b:", a**b)
+print("a//b:", a//b)
+print("20.0//3:", 20.0//3)
+
+
+
+     
+     
+     
+
+     
+     
+     
+     
+
+
