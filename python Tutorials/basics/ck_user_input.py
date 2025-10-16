e28782f6daf6bc602c4bc64f48512a05a5cd3833
Admin <Admin@DESKTOP-LQFA8J7>
2025-10-16 20:19:10 +0530

    ck_userinput

---
+++ python Tutorials/basics/ck_user_input.py
@@ -0,0 +1,14 @@
+'''
+Created on 16-Oct-2025
+
+@author:chandan 
+
+input()
+'''
+name =input("enter your name:")
+print("your name is:",name)
+# two user input
+a = int(input("enter the  first number :"))
+b = int(input("enter the second  number :"))
+print("the sum is:",a+b)
+
