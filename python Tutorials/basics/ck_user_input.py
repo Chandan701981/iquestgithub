6ab5203ef8f80c43f39a034bbe52ed85c55b2af9
Admin <Admin@DESKTOP-LQFA8J7>
2025-10-18 21:20:05 +0530

    ck_userinput

--- python Tutorials/basics/ck_user_input.py
+++ python Tutorials/basics/ck_user_input.py
@@ -5,10 +5,23 @@ Created on 16-Oct-2025
 
 input()
 '''
+'''
 name =input("enter your name:")
 print("your name is:",name)
 # two user input
 a = int(input("enter the  first number :"))
 b = int(input("enter the second  number :"))
 print("the sum is:",a+b)
+'''
+name1 =input("enter your first name:")
+name2 =input("enter your second name:")
+name3 =input("enter your third name:")
+print("your  first name is:",name1)
+print("your second name is:",name2)
+print("your third name is:",name3)
+
+
+
+
+
 
