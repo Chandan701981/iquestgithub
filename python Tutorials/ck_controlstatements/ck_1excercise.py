eda37610a8e3814968cdd5126ce9908a6eeac385
Admin <Admin@DESKTOP-LQFA8J7>
2025-10-20 17:01:02 +0530

    ck_1

---
+++ python Tutorials/ck_controlstatements/ck_1excercise.py
@@ -0,0 +1,15 @@
+'''
+Created on 20-Oct-2025
+
+@author: chandan
+'''
+num =int(input("enter the number:"))
+if num<0:
+     print("number is negative")
+elif num>0:
+     print("number is positive")
+elif num==0:
+     print("number is zero")
+else:
+    print("program terminated")    
+    
