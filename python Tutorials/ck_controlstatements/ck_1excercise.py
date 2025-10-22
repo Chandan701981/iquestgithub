81f8f70d0e97f95977b48feba1de85ed1fa7b243
Admin <Admin@DESKTOP-LQFA8J7>
2025-10-22 23:55:01 +0530

    excercise_1

--- python Tutorials/ck_controlstatements/ck_1excercise.py
+++ python Tutorials/ck_controlstatements/ck_1excercise.py
@@ -3,6 +3,8 @@ Created on 20-Oct-2025
 
 @author: chandan
 '''
+'''
+#check if a number is positive ,negative ,zero
 num =int(input("enter the number:"))
 if num<0:
      print("number is negative")
@@ -12,4 +14,51 @@ elif num==0:
      print("number is zero")
 else:
     print("program terminated")    
+    '''
+ 
+#find greatest of three numbers
+'''
+ 
+num1 =float(input("enter the  first numbers:"))
+num2 =float(input("enter the second number:"))
+num3 =float(input("enter the third number:"))
+if num1>=num2 and num1>=num3:
+    print(num1,"is  the greatest number")
+elif num2>=num1 and num2>=num3:
+    print(num2,"is the greatest number")
+else:
+    print(num3,"is the greatest number") 
+    '''
+    
+    
+         
+'''
+year =int(input("enter the year in YYYY format:"))
+if year%4 == 0:
+    print("this is a leap year")
+else:
+    print("this is not a leap year")
+    '''    
+               
+                
+ # display  grade based on student marks
+# Accept marks from the user
+marks = float(input("Enter your marks:"))
+
+if marks >= 90:
+    print("your grade is A+")
+elif marks >= 80:
+    print("your grade is A")
+elif marks >= 70:
+    print("your grade is B")
+else:
+    print("your grade is C")
+     
+       
+
+               
+     
+
+
+    
     
