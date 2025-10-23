c64c2d65fe3895f7899d5535b5e12181b13bbf2f
Admin <Admin@DESKTOP-LQFA8J7>
2025-10-23 21:18:59 +0530

    ck_looping_statements

---
+++ python Tutorials/ck_controlstatements/ck_looping_statements.py
@@ -0,0 +1,35 @@
+'''
+Created on 23-Oct-2025
+
+@author: chandan
+"reduce repetation - increase reuse" -->easy maintainence
+
+
+Looping statements: 
+-looping statemnts are used to executes any statement/s repeatedly.
+-any statements are used to executes any statement/s reapedly.
+-any statement/s will be executed untill a condition is fulfilled
+
+
+types of looping statements:
+1.While loop:
+   -initial variable---used to set initial value
+   -define the condition
+   -increment /decrement
+2.For loop
+
+
+'''
+'''
+count = 0 #initial variable
+
+while count < 5: # condition
+    print("Hello World!")
+    count = count + 1 #increment
+    count+=1 #increment
+    '''
+    
+count = 5
+while count > 0:
+    print("Hello World!")
+    count-=1
